dd16d01dbad42f4390ad26d6e2a7570a46baa295
yatessss <897969073@qq.com>
2017-08-15 23:07:48 +0800

    已经可以获取到用户的openid和头像等信息s

--- controllers/api.js
+++ controllers/api.js
@@ -3,6 +3,7 @@
  */
 const config = require('../config');
 const model = require('../model');
+const axios = require('axios')
 
 let Match = model.Match
 
@@ -34,5 +35,30 @@ module.exports = {
     console.log('创建了: ' + JSON.stringify(match));
 
     ctx.redirect(`/room/${room_id}`)
+  },
+
+  'GET /api/getCode': async (ctx, next) => {
+    console.log('接收到了网页发来的消息')
+    const appid = 'wx470ba9b3c2e89e1f'
+    const secret = 'd7232fd5d58f28245b803a72b8f185e2'
+    const code = ctx.request.query.code
+    let res = {}
+    await axios({
+      method:'get',
+      url:`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appid}&secret=${secret}&code=${code}&grant_type=authorization_code`,
+    }).then(function(response) {
+      console.log('发送成功了一个消息')
+      return response
+    }).then((response) => {
+      console.log(`https://api.weixin.qq.com/sns/userinfo?access_token=${response.data.access_token}&openid=${response.data.openid}&lang=zh_CN`,)
+      return axios({
+        method:'get',
+        url:`https://api.weixin.qq.com/sns/userinfo?access_token=${response.data.access_token}&openid=${response.data.openid}&lang=zh_CN`,
+      })
+    }).then((response)=> {
+      console.log('最后的数据', response)
+      res = response.data
+    });
+    ctx.rest(res);
   }
 };
